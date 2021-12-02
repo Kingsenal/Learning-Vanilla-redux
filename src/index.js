0ee410c22811f0d0f168601b4f79c12bfc1d49ae
Kingsenal <kwonddong2@gmail.com>
2021-12-02 14:56:18 +0900

    h

--- src/index.js
+++ src/index.js
@@ -1,13 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { createStore } from 'redux';
 
+const plus = document.getElementById("plus")
+const minus = document.getElementById("minus")
+const number = document.querySelector("span")
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+number.innerText = 0
 
+const countModifier = (count = 0, action) => {
+  switch (action.type) {
+    case "PLUS":
+      return count + 1
+    case "MINUS":
+     return count -1   
+    default:
+     return count;
+    }
+}
 
+const countStore = createStore(countModifier);
+
+const onChnage = () => {
+  number.innerText = countStore.getState();
+}
+
+countStore.subscribe(onChnage)
+
+const handlePlus = () => {
+  countStore.dispatch({ type: "PLUS" })
+}
+
+const handleMinus = () => {
+  countStore.dispatch({ type: "MINUS" })
+}
+
+plus.addEventListener("click", handlePlus);
+minus.addEventListener("click", handleMinus);
